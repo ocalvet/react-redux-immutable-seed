e8bacbb37062119d459929611adea7c433dbb05c
Ovidio R. Calvet <calveto@gmail.com>
2018-08-12 23:33:42 -0400

    Switch to describe it block

--- src/App.spec.js
+++ src/App.spec.js
@@ -3,10 +3,16 @@ import React from 'react';
 import App from './App';
 import ShallowRenderer from 'react-test-renderer/shallow'
 
-test('App renders correctly', () => {
-  const renderer = new ShallowRenderer();
-  const result = renderer.render(
-    <App />,
-  );
-  expect(result).toMatchSnapshot();
-});
+describe('App', () => {
+  it('Matches the old snapshot', () => {
+    const renderer = new ShallowRenderer();
+    const result = renderer.render(
+      <App />,
+    );
+    expect(result).toMatchSnapshot();
+  });
+  
+  it('renders a div with the container class', () => {
+    expect(false).toBeFalsy();
+  })
+})
